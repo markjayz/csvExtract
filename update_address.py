29f7a6c9cd80e3de4ccef8dbff350528db5272f2
jacruz-sc <jacruz@quadx.xyz>
2022-08-24 16:08:25 +0800

    feature/SC-4028 - modified update address script
    Signed-off-by: jacruz-sc <jacruz@quadx.xyz>

--- update_address.py
+++ update_address.py
@@ -5,16 +5,16 @@ import requests, json
 import csv
 from datetime import date
 
-workbook = pd.read_csv('D:/Work/Files/QuadX/Templates/sample_csv.csv')
+workbook = pd.read_csv('D:/Work/Files/QuadX/Templates/sample_staging.csv')
 today = date.today()
-with open(f'{today}_created-address.csv', 'w', encoding='UTF8') as f:
+with open(f'{today}_created-address-staging-2.csv', 'w', encoding='UTF8') as f:
     writer = csv.writer(f)
     header = ['Party ID', 'Line 1', 'Barangay', 'City',  'State', 'Postal Code', 'Country Code', 'Region']
      # write the header
     writer.writerow(header)
     def my_function():
         for index, row in workbook.iterrows():
-                url = "https://api.staging.quadx.xyz/v1/locations/provinces/"
+                url = "https://api.quadx.xyz/v1/locations/provinces/"
                 headers = {
                         'Content-Type': 'application/json'
                     }  
@@ -34,7 +34,7 @@ with open(f'{today}_created-address.csv', 'w', encoding='UTF8') as f:
                             city_name = city['name']
                             if row['city'].lower() in city_name.lower():
                                 city_id = city['id']
-                                url_district = 'https://api.staging.quadx.xyz/v1/locations/cities/' + str(city_id) + '/districts'
+                                url_district = 'https://api.quadx.xyz/v1/locations/cities/' + str(city_id) + '/districts'
                                 response_district = requests.get(url_district, headers=headers)
                                 data_district = json.loads(response_district.text)   
 
@@ -44,15 +44,16 @@ with open(f'{today}_created-address.csv', 'w', encoding='UTF8') as f:
                                             district_code = district['postal_code']
                                             district_name = district['name']
                                     
-                                            update_url = "https://sc-api.staging.shippingcart.com/api/addresses/" + str(row['id']) + "/update"
+                                            update_url = "https://sc-api.shippingcart.com/api/addresses/" + str(row['id']) + "/update"
                                             update_headers = {
+                                                    'Connection': 'keep-alive',
+                                                    "Accept-Encoding": "*",
                                                     'Content-Type': 'application/json',
                                                     'authorization': 'Bearer {{token}}'
                                                 }
                                             payload = {
                                                 "data": {
                                                     "party_id": row['party_id'],
-                                                    "line_1": row['line_1'],
                                                     "line_2": district_name,
                                                     "city": city_name,
                                                     "postal_code": district_code,
@@ -63,9 +64,9 @@ with open(f'{today}_created-address.csv', 'w', encoding='UTF8') as f:
                                                 }
                                             }
                                             response_update = requests.put(update_url, headers=update_headers, data=json.dumps(payload))
-                                            data_update  = json.loads(response_update.text) 
-                                            data_csv = [row['party_id'], row['line_1'], district_name, city_name,  province_name, district_code, "PH", region]
-                                            # data_csv = [data_update['party_id'], data_update['line_1'], data_update['line_2'], data_update['city'],  data_update['state'], data_update['postal_code'], data_update['country_code'], data_update['region']]
+                                            data_update  = json.loads(response_update.text)
+                                            # data_csv = [row['party_id'], row['line_1'], district_name, city_name,  province_name, district_code, "PH", region]
+                                            data_csv = [data_update['party_id'], data_update['line_1'], data_update['line_2'], data_update['city'],  data_update['state'], data_update['postal_code'], data_update['country_code'], data_update['region']]
                                             print(data_csv)
                                             writer.writerow(data_csv)
     my_function()
